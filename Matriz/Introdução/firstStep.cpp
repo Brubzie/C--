13f53e8c906dd45de2050e59d682a88f1aed2918
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2023-12-13 18:43:43 -0300

    Criação do código de estudos para Matriz

--- Matriz/Introdução/firstStep.cpp
+++ Matriz/Introdução/firstStep.cpp
@@ -24,7 +24,57 @@ int main() {
 
     setlocale(LC_ALL, "Portuguese");
 
+    // int matriz[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
+    int menu = 0, linhas = 0, colunas = 0;
+
+    cout << "Defina a quantidade de linhas da matriz: ";
+    cin >> linhas;
+
+    cout << "Defina a quantidade de colunas da matriz: ";
+    cin >> colunas;
+
+    int matriz[linhas][colunas];
+
+    do {
+        clean();
+
+        cout << "=== Matriz ===" << endl
+             << "1 - Adicionar valor para as " << colunas << " coluna(s)" << endl
+             << "2 - Mostrar matriz" << endl
+             << "3 - Sair" << endl
+             << "Opção: ";
+        cin >> menu;
+
+        clean();
+
+        switch (menu) {
+            case 1: {
+                for (int i = 0; i < linhas; ++i) {
+                    for (int j = 0; j < colunas; ++j) {
+                        cout << "Matriz[ " << i << " ][ " << j << " ]: ";
+                        cin >> matriz[i][j];
+                    }
+                }
+            } break;
+            case 2: {
+                for (int i = 0; i < 2; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        cout << matriz[i][j] << " | ";
+                    }
+                    cout << endl;
+                }
+                pause();
+            } break;
+            case 3: {
+                cout << "Encerrando..." << endl;
+            } break;
+            default: {
+                cout << "[ERRO] Opção inválida! Insira outro valor.";
+                pause();
+            } break;
+        }
+    } while (menu != 3);
 
     return 0;
 }
