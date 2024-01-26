c805222f28d92fb310570a358aa248b3ac89a3c7
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2024-01-25 23:05:10 -0300

    continuação do projeto

--- Práticas Gerais/Gerador de PDF/WEEK_HEADER.h
+++ Práticas Gerais/Gerador de PDF/WEEK_HEADER.h
@@ -4,16 +4,19 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <ctime>
+#include <cstdlib>
 
 //Estruturas
-struct Usuario {
-    std::string nomeDeUsuario;
-};
-
 struct Node {
-    Usuario usuario;
+    Tarefa tarefa;
     Node* proximo;
     Node* anterior;
+};
+
+struct Tarefa {
+    std::string descricao;
+    std::string horario;
 };
 
 struct ListaDeTarefas {
@@ -24,10 +27,10 @@ struct ListaDeTarefas {
 
 // Funções
 void limpar() {
-    #ifdef _WIN32 // <= Identifica se o SO é Windows
+    #ifdef _WIN32 // Identifica se o SO é Windows
         system("cls");
     #else
-        system("clear");
+        system("clear"); // Caso for um SO Linux, MacOS ou demais
     #endif
 }
 
@@ -60,8 +63,41 @@ void destruir(ListaDeTarefas* lista) {
     }
 }
 
-void cadastroDeTarefa(ListaDeTarefas* lista, Usuario* usuario) {
-    Node* novo = new Node{ *usuario, nullptr, nullptr };
+int geradorDeSenhas(int senha) {
+    senha = 0;
+
+    // Definindo a seed(semente) para a função rand()
+    srand(time(NULL));
+
+    // Gera a senha aleatória
+    for (int i = 0; i < 8; i++) {
+        int digito = rand() % 10;
+        senha += digito;
+    }
+
+    // Imprime a senha gerada
+    std::cout << "Senha gerada: " << senha << std::endl;
+
+    return senha;
+}
+
+int senhaCriada(int senha) {
+    senha = 0;
+
+    // Definindo a senha
+    std::cout << "Informe a senha: ";
+    std::cin >> senha;
+
+    return senha;
+}
+
+bool verificarFormatoHorario(string horario) {
+    regex padrao("[0-9]{2}:[0-9]{2}");
+    return regex_match(horario, padrao);
+}
+
+void adicionarTarefa(ListaDeTarefas* lista, Tarefa* tarefa) {
+    Node* novo = new Node{ *tarefa, nullptr, nullptr };
 
     if (lista->primeiro == nullptr) {
         lista->primeiro = novo;
@@ -76,10 +112,31 @@ void cadastroDeTarefa(ListaDeTarefas* lista, Usuario* usuario) {
 
     std::cin.ignore(numeric_limits<streamsize>::max(),'\n');
 
-    std::cout << "Informe sua tarefa: ";
-    std::getline(cin, tarefas);
+    std::cout << "Informe a descrição da tarefa: ";
+    std::getline(cin, lista->ultimo->tarefa.descricao);
 
-    arquivo << tarefas << std::endl;
+    std::cout << "Informe o horário da tarefa (no formato HH:MM): ";
+    std::getline(cin, lista->ultimo->tarefa.horario);
+
+    if (verificarFormatoHorario(lista->ultimo->tarefa.horario)) {
+        limpar();
+        std::cout << "Tarefa adicionada com sucesso!" << std::endl;   
+    } else {
+        std::cout << std::endl << "O horário não está no formato correto." << std::endl;
+    }
+    pausar();
+}
+
+void listarTarefa(ListaDeTarefas* lista) {
+    if (lista->tamanho = 0) {
+        std::cout << "Nenhuma tarefa encontrada." << std::endl;
+    } else {
+        std::cout << "Lista de tarefas: " << std::endl;
+        for (Node* aux = lista->primeiro; aux != nullptr; aux = aux->proximo) {
+            std::cout << "- " << aux->tarefa.descricao << " (" << aux->tarefa.horario << ") " << std::endl;
+        }
+    }
+    pausar();
 }
 
 #endif
--- Práticas Gerais/Gerador de PDF/week.cpp
+++ Práticas Gerais/Gerador de PDF/week.cpp
@@ -2,15 +2,17 @@
 #include <string>
 #include <locale>
 #include <fstream>
+#include <ctime>
+#include <cstdlib>
 #include "WEEK_HEADER.h" // Arquivo de cabeçalho
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
-    std::string tarefas;
-    std::ofstream arquivo;
+    ListaDeTarefas lista;
+    inicializar(&lista);
 
-    int menu;
+    std::ofstream arquivo;
 
     arquivo.open("tarefas.txt", ios::app);
 
@@ -19,6 +21,8 @@ int main() {
         return 1;
     }
 
+    int menu = 0;
+
     do {
         limpar();
 
@@ -26,8 +30,7 @@ int main() {
                   << "1 - Adicionar Tarefa" << std::endl
                   << "2 - Remover Tarefa" << std::endl
                   << "3 - Editar Tarefa" << std::endl
-                  << "4 - Definir dia como 'livre'" << std::endl
-                  << "5 - Sair" << std::endl
+                  << "4 - Sair" << std::endl
                   << "Opção: ";
         std::cin >> menu;
 
@@ -35,50 +38,7 @@ int main() {
 
         switch (menu) {
             case 1: {
-                int dia;
-
-                std::cout << "1 - Segunda" << std::endl
-                          << "2 - Terça" << std::endl
-                          << "3 - Quarta" << std::endl
-                          << "4 - Quinta" << std::endl
-                          << "5 - Sexta" << std::endl
-                          << "6 - Sábado" << std::endl
-                          << "7 - Domingo" << std::endl << std::endl;
-                          << "Informe o dia da semana para a tarefa: ";
-                std::cin >> dia;
-
-                limpar();
-
-                switch (dia) {
-                    case 1: { // Segunda
-                        cadastroDeTarefa();
-
-                        break;
-                    }
-                    case 2: { // Terça
-                        break;
-                    }
-                    case 3: { // Quarta
-                        break;
-                    }
-                    case 4: { // Quinta
-                        break;
-                    }
-                    case 5: { // Sexta
-                        break;
-                    }
-                    case 6: { // Sábado
-                        break;
-                    }
-                    case 7: { // Domingo
-                        break;
-                    }
-                    default: { // Opção inválida
-                        std::cout << "[ERRO] Informe um número de 1 a 7, correspondente ao dia da semana que queira adicionar sua tarefa." << std::endl;
-                        pausar();
-                        break;
-                    }
-                }
+                adicionarTarefa(&lista, &lista.ultimo->tarefa);
 
                 break;
             }
@@ -96,21 +56,14 @@ int main() {
                 break;
             }
             default: {
-                std::cout << "[ERRO] Insira um número entre 1 e 5." << std::endl;
+                std::cout << "[ERRO] Insira um número entre 1 e 4." << std::endl;
                 pausar();
                 break;
             }
         }
     } while (menu != 4);
 
-    // std::cout << "Digite sua tarefa: ";
-    // std::getline(cin, tarefas);
-
-    // arquivo << tarefas << std::endl;
-
-    // arquivo.close();
-
-    // cout << "Tarefa salva com sucesso!" << std::endl;
+    destruir(&lista);
 
     return 0;
 }
